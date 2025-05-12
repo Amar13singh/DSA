14e3115ead861e89f6f175e82f3ec00f6af550e4
Amar13singh <amarkumarkrishna11@gmail.com>
2025-05-12 22:44:42 +0530

    Binary-Search

---
+++ Binary-Search/rSortedArr.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        // Left half is sorted
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+
+    return -1; // Not found
+}
+ int main() {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    int index = search(nums, target);
+    cout << "Index: " << index << endl;
+    return 0;
+}
