ebdb2d55cca9961c7c0a65f1f49436714951a10f
Amar13singh <amarkumarkrishna11@gmail.com>
2025-05-15 21:22:28 +0530

    bs

---
+++ Binary-Search/koko.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int getHours(const vector<int>& piles, int k) {
+    int hours = 0;
+    for (int pile : piles) {
+        hours += (pile + k - 1) / k;
+    }
+    return hours;
+}
+
+
+int minEatingSpeed(vector<int>& piles, int h) {
+    int low = 1, high = *max_element(piles.begin(), piles.end());
+    int answer = high;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        int totalHours = getHours(piles, mid);
+
+        if (totalHours <= h) {
+            answer = mid;
+            high = mid - 1; // try smaller k
+        } else {
+            low = mid + 1;  // need bigger k
+        }
+    }
+    return answer;
+}
+
+int main() {
+    vector<int> piles = {30, 11, 23, 4, 20};
+    int h = 5;
+    int result = minEatingSpeed(piles, h);
+    cout << "Minimum eating speed: " << result << endl;
+    return 0;
+}
+
+
+
