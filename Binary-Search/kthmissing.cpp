279916014c1bec8ca91200bf51b2330d0ba4c0b0
Amar13singh <amarkumarkrishna11@gmail.com>
2025-05-17 15:01:59 +0530

    Day18

---
+++ Binary-Search/kthmissing.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int i = 0, num = 1;
+        while (k > 0) {
+            if (i < arr.size() && arr[i] == num) {
+                i++;
+            } else {
+                k--;
+                if (k == 0) return num;
+            }
+            num++;
+        }
+        return -1; // should never reach
+    }
+};
+
+int main(){
+Solution solution;
+    vector<int> arr = {2, 3, 4, 7, 11};
+    int k = 5;
+    int result = solution.findKthPositive(arr, k);
+    cout << "The " << k << "th missing positive integer is: " << result << endl;
+}
---
+++ Binary-Search/kthmissing1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findKthPositive(vector<int>& arr, int k) {
+    int low = 0, high = arr.size() - 1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(arr[mid] - (mid + 1) < k){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return low + k;
+
+}
+
+int main(){
+    vector<int> arr = {2, 3, 4, 7, 11};
+    int k = 5;
+    int result = findKthPositive(arr, k);
+    cout << "The " << k << "th missing positive integer is: " << result << endl;
+}
