f185544e2a419ea5a81f19d6c51dcc10d519a401
Amar13singh <amarkumarkrishna11@gmail.com>
2025-06-08 13:47:56 +0530

    360

---
+++ Recursion/generateParenthesis.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void backtrack(int open, int close, int n, string current, vector<string>& result) {
+    if (current.length() == 2 * n) {
+        result.push_back(current);
+        return;
+    }
+
+    if (open < n) {
+        backtrack(open + 1, close, n, current + '(', result);
+    }
+    if (close < open) {
+        backtrack(open, close + 1, n, current + ')', result);
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> result;
+    backtrack(0, 0, n, "", result);
+    return result;
+}
+
+int main() {
+    int n = 3;
+    vector<string> ans = generateParenthesis(n);
+    for (const string& s : ans) {
+        cout << s << endl;
+    }
+    return 0;
+}
