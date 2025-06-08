0f246f21eaf52492d20419a1708a5349c6e56e0f
Amar13singh <amarkumarkrishna11@gmail.com>
2025-06-08 13:02:13 +0530

    Day 36

---
+++ Recursion/generateBinary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void generateBinary(int n, string current) {
+    if (n == 0) {
+        cout << current <<" ";
+        return;
+    }
+    generateBinary(n - 1, current + '0');
+    generateBinary(n - 1, current + '1');
+}
+
+int main() {
+    int n = 3; // change this value to generate strings of different lengths
+    generateBinary(n, "");
+    return 0;
+}
