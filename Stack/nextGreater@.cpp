d66aaea6a297184234541f4573f0a34828c23bec
Amar13singh <amarkumarkrishna11@gmail.com>
2025-06-25 17:22:44 +0530

    460

---
+++ Stack/nextGreater@.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> result(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && nums[st.top()] < nums[i]) {
+            result[st.top()] = nums[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {4, 5, 2, 10, 8};
+    vector<int> result = nextGreaterElement(nums);
+    cout << "Next Greater Elements: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
