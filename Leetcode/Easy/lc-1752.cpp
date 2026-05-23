966d56425eebee0b10cf904e73a00806d2af3b79
Amar13singh <amarkumarkrishna11@gmail.com>
2026-05-23 22:53:45 +0530

    leetcode rotated arr Qs

---
+++ Leetcode/Easy/lc-1752.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//works even on duplicates...
+
+bool rotatedAndSorted(vector<int>& arr){
+    int n = arr.size();
+    int cnt = 0;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[(i+1)%n]) cnt++;
+    }
+    return cnt>1?false:true;
+}
+
+int main(){
+    vector<int> nums = {3,4,5,1,2,2,3};
+    cout<<rotatedAndSorted(nums);
+}
+
+
