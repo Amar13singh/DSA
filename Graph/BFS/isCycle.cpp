a6779ee01d39732225c1d286d6130d98e50f8e90
Amar13singh <amarkumarkrishna11@gmail.com>
2026-06-07 11:10:00 +0530

    cycle detection methods

--- Graph/BFS/isCycle.cpp
+++ Graph/BFS/isCycle.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool dfs(vector<vector<int>>& mat,int start,vector<int>& vis){
+
+//cycle detection using bfs ....
+bool bfs(vector<vector<int>>& mat,int start,vector<int>& vis){
     queue<pair<int,int>> q;
     vis[start]= 1;
     q.push({start,-1});
@@ -13,7 +15,7 @@ bool dfs(vector<vector<int>>& mat,int start,vector<int>& vis){
         for(int x:mat[node]){
             if(!vis[x]){
                 vis[x] = 1;
-                q.push({x,node})
+                q.push({x,node});
             }
             else if(x!=parent) return true;
         }
@@ -23,8 +25,7 @@ bool dfs(vector<vector<int>>& mat,int start,vector<int>& vis){
 }
 
 
-
-bool isCycle(vector<vector<int>> &mat,int v){
+bool isCycle(vector<vector<int>> &mat){
     int n = mat.size();
     vector<int> vis(n,0);
 
@@ -39,5 +40,28 @@ bool isCycle(vector<vector<int>> &mat,int v){
 }
 
 int main(){
-    
+    vector<vector<int>> adj =
+    {
+        {1,3},   // 0
+        {0,2},   // 1
+        {1,3},   // 2
+        {0,2}    // 3
+    };
+
+    vector<vector<int>> adj1 =
+{
+    {1},        // 0
+    {0,2},      // 1
+    {1},        // 2
+
+    {4,5},      // 3
+    {3,5},      // 4
+    {3,4}       // 5
+};
+
+    cout << (isCycle(adj1) ? "Cycle Found" : "No Cycle");
+
+    return 0;
+
+
 }
