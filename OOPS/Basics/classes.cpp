5a1a26e69986c5836d525f7107786d15c252d417
Amar13singh <amarkumarkrishna11@gmail.com>
2026-07-10 19:04:37 +0530

    revising oops

---
+++ OOPS/Basics/classes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Dog{                         
+    public:                        //Access specifiers
+        string breed, color;       //Data members
+}; 
+
+void main(){
+
+//We can access these data members breed and color by (.) operator.
+
+Dog dog;                //declaring object of class Dog
+dog.breed="Pug";        //accessing public data members
+dog.color="Brown";
+
+}
