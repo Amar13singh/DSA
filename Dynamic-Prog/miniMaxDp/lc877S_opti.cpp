bcbb9b2ee765454941ccced430738fba565954b4
Amar13singh <amarkumarkrishna11@gmail.com>
2026-08-04 12:23:10 +0530

    minimax dp

---
+++ Dynamic-Prog/miniMaxDp/lc877S_opti.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dp;
+
+//space optimized for stone game problem 
+//before this it is necessary to implement tabulation method 
+// we just compress 2d/3d to 1d/2d and saave space if possible...
+
+bool stoneGame(vector<int>& p){
+    int n = p.size();
+    dp = p;
+
+    for()
+
+    
+
+}
--- Dynamic-Prog/miniMaxDp/tabulation.cpp
+++ Dynamic-Prog/miniMaxDp/tabulation.cpp
@@ -13,14 +13,37 @@ vector<vector<int>> dp;
 
 bool stoneGame(vector<int>& p){
     int n = p.size();
+    dp.assign(n,vector<int>(n,INT_MIN));
+
 
     //base cases..
-    for(int i =0;i<n;i++){
+    for(int i=0;i<n;i++){
         dp[i][i] = p[i];
+    } 
+
+     //diagonal is base case actually that why when we traverse and store ans in dp 
+    // the value l<r very imp bcz other than this become invalid...
+
+
+    //very important in tabulation is to visualise filing order in table and acc
+    // decide to use loop range ,even slight mistake can make sol wrong...
+
+    for(int l=n-1;l>=0;l--){
+        for(int r=l+1;r<n;r++){
+            dp[l][r] = max(
+                p[l]-dp[l+1][r],
+                p[r]-dp[l][r-1]
+            );
+        }
     }
 
-    for(int i = n-2;i>=0;i--){
-        for(int j=i)
-    }
+    return dp[0][n-1]>0;
 
+}
+
+int main(){
+    vector<int> arr ={5,3,4,5};
+
+
+    stoneGame(arr)?cout<<"yes":cout<<"No";
 }
